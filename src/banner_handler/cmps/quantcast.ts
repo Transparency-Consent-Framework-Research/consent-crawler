31371587f5bdae1a0719308dffbb12ad6a2f6ef7
Pritam Sen <pritam.sen.2406@gmail.com>
2023-03-23 18:14:06 -0400

    Updating rules for Quantcast

--- src/banner_handler/cmps/quantcast.ts
+++ src/banner_handler/cmps/quantcast.ts
@@ -4,19 +4,23 @@ import { BannerHandler } from '../index.js';
 export const quantcastHandler: BannerHandler = {
   name: 'Quantcast',
   url: 'quantcast.mgr.consensu.org',
-  preActionHook: async (page: Page) => {
-    console.log('Running pre-action hook');
-    await page.locator('#qc-cmp2-container .qc-cmp-cleanslate .qc-cmp2-summary-buttons button[mode="secondary"]').click();
-  },
+  // preActionHook: async (page: Page) => {
+  //   console.log('Running pre-action hook');
+  //   await page.locator('#qc-cmp2-container .qc-cmp-cleanslate .qc-cmp2-summary-buttons button[mode="secondary"]').click();
+  // },
   variants: [
     {
-      name: 'Header Links',
-      check: async(page: Page) => !!(await page.locator('#qc-cmp2-container .qc-cmp-cleanslate .qc-cmp2-header-links').count()),
-      accept: async (page: Page) => (await page.locator('#qc-cmp2-container .qc-cmp-cleanslate .qc-cmp2-summary-buttons button:first-child').click()),
+      name: 'Variant - Footer/PopUp',
+      check: async(page: Page) => !!(await page.locator('#qc-cmp2-container .qc-cmp2-footer').count()),
+      accept: async (page: Page) => (await page.locator('#qc-cmp2-container .qc-cmp2-footer button[mode="primary"]').click()),
       reject: async (page: Page) => {
-        await page.locator('#qc-cmp2-container .qc-cmp-cleanslate .qc-cmp2-header-links button:first-child').click();
+        await page.locator('#qc-cmp2-container .qc-cmp2-footer button:first-child').click();
         await page.waitForTimeout(500);
-        await page.locator('.qc-cmp2-buttons-desktop button[mode="primary"]').click();
+        const moreOptionPopUp = await page.locator('.qc-cmp2-consent-info .qc-cmp2-header-links button:first-child').count();
+        if(moreOptionPopUp > 0) {
+          await page.locator('.qc-cmp2-consent-info .qc-cmp2-header-links button:first-child').click();
+        }
+        console.log('Rejected Succesfully.');
       }
     },
     {
